ee5d46171f54ec25448e754de7c800eaddf096b2
phobal <phobal@126.com>
2017-12-09 22:23:15 +0800

    update: 由于原网站挂了，替换了新的网站地址

--- file.js
+++ file.js
@@ -3,7 +3,7 @@ const SS = require('./ss');
 
 exports.readFile = function() {
   const now = Date.parse(new Date());
-  new SS({ url: `https://ss.weirch.com/ss.php?_=${now}` });
+  new SS({ url: `https://free-ss.site/ss.php?_=${now}` });
   const data = fs.readFileSync('./ss.json', 'utf-8');
   return JSON.parse(data).configs;
 };
